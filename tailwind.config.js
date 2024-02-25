164851def1ab6a5db0416fc1a534dc2d4ce41052
herve1983 <87281704+herve1983@users.noreply.github.com>
2024-02-25 08:31:36 +0100

    feature/Install-nextui: Fix NextUI missing theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    // Theme for NextUI !!!!!
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 
     // Or if using `src` directory:
